043f57feda999554ccbf8901fc7f1be9bad8b69b
Aneesh Saravu Karekad <saravukarekadaneesh@gmail.com>
2023-12-02 14:41:38 -0500

    feat: setup husky for continuous integration

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -2,7 +2,10 @@ import { describe, expect, it } from 'vitest'
 import main from '../src'
 
 describe('[ Operation: Root suite ]', () => {
-  it('should be a function', () => {
+  it('should be a function', async () => {
     expect(typeof main).toBe('function')
+    expect(async () => {
+      await main('world')
+    }).not.toThrow()
   })
 })
